3fef8f0b807ae6175188877faebeb2eea17c91fe
“Mia <miahofmann2@gmail.com>
2021-08-07 14:22:58 -0400

    RPS buttons

--- myscript.js
+++ myscript.js
@@ -5,31 +5,75 @@ var compScore = 0;
 var playerScore = 0;
 
 
-   //computer returns rock, paper, or scissors
+   //computer returns randomized rock, paper, or scissors
    function computerPlay(){
        computerSelection = (array1[Math.floor(Math.random() * array1.length)]);
        return computerSelection;
    }
 
    //takes input from player who types in rock, paper, or scissors
-   function playerPlay(){
-       playerSelection = prompt("Rock, Paper, or Scissors?", "");
-       playerSelection = playerSelection.trim();
-       playerSelection = playerSelection.toLowerCase();
+//    function playerPlay(){
+//        playerSelection = prompt("Rock, Paper, or Scissors?", "");
+//        playerSelection = playerSelection.trim();
+//        playerSelection = playerSelection.toLowerCase();
 
-       if (playerSelection === "rock" || playerSelection === "scissors" || playerSelection === "paper"){
-        return playerSelection;
-       }
-       else{
-           alert("Invalid input, try again!")
-       }
-   }
+//        if (playerSelection === "rock" || playerSelection === "scissors" || playerSelection === "paper"){
+//         return playerSelection;
+//        }
+//        else{
+//            alert("Invalid input, try again!")
+//        }
+//    }
+
+
+//UI
+
+const buttons = document.querySelectorAll('input')
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value, computerPlay());
+    })
+})
+// const rockBtn = document.querySelector('.rockBtn');
+// const paperBtn = document.getElementById('paperBtn');
+// const scissorsBtn = document.getElementById('scissorsBtn');
+
+// rockBtn.addEventListener('click', () => handleClick('ROCK'));
+// paperBtn.addEventListener('click', () => handleClick('PAPER'));
+// scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'));
+
+
+// function handleClick(playerSelection){
+//     computerPlay() = computerSelection;
+//     playRound(playerSelection, computerSelection);
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+// }
+
+//    var playerPlay = function(){
+//     if (playerSelection.getElementsId('rockBtn') ){
+//         playerSelection = "rock";
+//         return playerSelection;
+//         console.log(playerSelection);
+//         }
+//     else if (playerSelection.getElementsById('paperBtn') ){
+//         playerSelection = "paper";
+//         return playerSelection;
+//         }
+//     else if (playerSelection.getElementsById('scissorsBtn') ){
+//         playerSelection = "scissors";
+//         return playerSelection;
+//         }   
+//    };
+
+   
 
    // Plays a single round of Rock Paper Scissors + returns a string that declares the winner
    function playRound(playerSelection, computerSelection) {
        
-        computerSelection = computerPlay();
-        playerSelection = playerPlay();
+        //computerSelection = computerPlay();
+        //playerSelection = playerPlay();
 
        if (playerSelection === "rock" && computerSelection === "paper"){
         alert("You Lose! Paper beats Rock");
@@ -85,4 +129,4 @@ var playerScore = 0;
     // }
 
 
-        console.log(game());
+       // console.log(game());
